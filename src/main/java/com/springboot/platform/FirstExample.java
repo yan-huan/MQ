fac31da0a1571b92fc95ae55cf61877b1df4d22d
yanhuan <huan.yan@tcredit.com>
2018-02-11 11:08:00 +0800

    Initial commit

---
+++ src/main/java/com/springboot/platform/FirstExample.java
@@ -0,0 +1,22 @@
+package com.springboot.platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController  
+@EnableAutoConfiguration  
+public class FirstExample {  
+  
+    @RequestMapping("/")  
+    String home() {  
+        return "Hello World！世界你好！O(∩_∩)O哈哈~！！！";  
+    }  
+  
+    public static void main(String[] args) {  
+  
+        SpringApplication.run(FirstExample.class, args);  
+    }  
+}  
